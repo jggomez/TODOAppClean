5b6da3e6da15dae6a2e4e1f24abfdafa601244f4
jggomezt <juan.gomez01@gmail.com>
2018-04-06 19:19:25 -0500

    se sube la primera version

---
+++ app/src/main/java/co/edu/ucc/todoapp/view/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package co.edu.ucc.todoapp.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.edu.ucc.todoapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/co/edu/ucc/todoapp/view/viewmodels/TaskViewModel.java
@@ -0,0 +1,27 @@
+package co.edu.ucc.todoapp.view.viewmodels;
+
+/**
+ * Created by jggomez on 23-Mar-18.
+ */
+
+public class TaskViewModel {
+
+    private String name;
+    private String uriImage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUriImage() {
+        return uriImage;
+    }
+
+    public void setUriImage(String uriImage) {
+        this.uriImage = uriImage;
+    }
+}
