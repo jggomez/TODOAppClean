b41fd3be94f9a246155f38059769b349bdf3ff3c
jggomezt <juan.gomez01@gmail.com>
2018-06-01 17:34:24 -0500

    se agrega menu

--- app/src/main/java/co/edu/ucc/todoapp/view/activities/MainActivity.java
+++ app/src/main/java/co/edu/ucc/todoapp/view/activities/MainActivity.java
@@ -1,12 +1,21 @@
 package co.edu.ucc.todoapp.view.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.List;
 
@@ -29,13 +38,23 @@ import co.edu.ucc.todoapp.view.viewmodels.mapper.TaskViewModelMapper;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity implements IMainView {
+public class MainActivity extends AppCompatActivity implements IMainView,
+        NavigationView.OnNavigationItemSelectedListener {
 
     @BindView(R.id.txtDescTask)
     EditText txtDescTask;
 
     @BindView(R.id.rvTask)
     RecyclerView rvTask;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.nav_view)
+    NavigationView nav_view;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer_layout;
 
     private IMainPresenter presenter;
     private TaskAdapter taskAdapter;
@@ -46,6 +65,15 @@ public class MainActivity extends AppCompatActivity implements IMainView {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24px);
+        }
+
+        nav_view.setNavigationItemSelectedListener(this);
 
         presenter = new MainPresenter(
                 this,
@@ -112,13 +140,59 @@ public class MainActivity extends AppCompatActivity implements IMainView {
     @OnClick(R.id.fbAddTask)
     @Override
     public void addTask() {
-        String descTask = txtDescTask.getText().toString();
-        presenter.addTask(descTask, false);
+        /*String descTask = txtDescTask.getText().toString();
+        presenter.addTask(descTask, false);*/
+
+        boolean wrapInScrollView = true;
+        new MaterialDialog.Builder(this)
+                .title(R.string.title)
+                .customView(R.layout.popu_task, wrapInScrollView)
+                .positiveText(R.string.positive)
+                .show();
     }
 
     @Override
     public void showTasks(List<TaskViewModel> taskViewModels) {
         taskAdapter.addLstTask(taskViewModels);
         taskAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_todo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+            Toast.makeText(this, "Click en Settings", Toast.LENGTH_SHORT).show();
+        }
+
+        if (id == android.R.id.home) {
+            drawer_layout.openDrawer(GravityCompat.START);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.contactar) {
+            Toast.makeText(this, "Click en Contactar del menu", Toast.LENGTH_SHORT).show();
+        }
+
+        if (id == R.id.acerca) {
+            Toast.makeText(this, "Click en Acerca del menu", Toast.LENGTH_SHORT).show();
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
